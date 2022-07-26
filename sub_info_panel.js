414483da2dc58f2ececad3302138fd203a912d94
Hochiun <66057006+Hochiun@users.noreply.github.com>
2022-07-26 13:42:00 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -33,9 +33,9 @@ Sub_info = script-name=Sub_info,update-interval=86400
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
-  let content = [`重置：剩余${resetDayLeft}天`];
-  let content = [`到期：${formatTime(expire)}`];
+  let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}
+  重置：剩余${resetDayLeft}天
+  到期：${formatTime(expire)}`];
 
 /*
   if (resetDayLeft) {
